160be9b7f9cce756d91e762ff471c288c342f1f9
Kaushal_Jha_Mac <kushal_jha_mac@Kaushals-Mac.local>
2024-10-20 12:51:24 +0530

    Input add in local storage

---
+++ index.js
@@ -0,0 +1,33 @@
+function loadToDos () {
+    const data = JSON.parse(localStorage.getItem('toDos')) || {list:[]};
+    console.log(data);
+    return data;
+}
+
+function addToLocalStorage (newToDo) {
+    const todos = loadToDos();
+    todos.list.push(newToDo);
+    let res = JSON.stringify(todos);
+    localStorage.setItem('toDos', res);
+}
+
+document.addEventListener('DOMContentLoaded',()=>{
+    console.log('Dom loaded');
+    const todoInput = document.getElementById("new-todo");
+    todoInput.addEventListener("change",(data)=>{
+        console.log("DATA===>",data.target.value);
+        const val = data.target.value;
+        data.target.value = val.trim();
+    });
+    const submitButton = document.getElementById("add-btn");
+    submitButton.addEventListener("click",()=>{
+        const data = todoInput.value.trim();
+        if(data=== ''){
+            alert("Please write something:");
+        }else {
+            addToLocalStorage(data);
+            todoInput.value="";
+        }
+    })
+    loadToDos();
+})
