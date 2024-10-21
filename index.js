960466513baf6d8f24c0d22ae6abd1d82b998e85
Kaushal_Jha_Mac <kushal_jha_mac@Kaushals-Mac.local>
2024-10-21 08:36:25 +0530

    Filter button apply

--- index.js
+++ index.js
@@ -6,9 +6,16 @@ function loadToDos () {
 
 function addToLocalStorage (newToDo) {
     const todos = loadToDos();
-    todos.list.push(newToDo);
+    todos.list.push({ text : newToDo, isCompleted : false });
     let res = JSON.stringify(todos);
     localStorage.setItem('toDos', res);
+}
+
+function appendToDoInHtml (todoText) {
+    const todoList = document.getElementById('todo-list');
+    const element = document.createElement('li');
+    element.textContent = todoText;
+    todoList.appendChild(element);
 }
 
 document.addEventListener('DOMContentLoaded',()=>{
@@ -25,9 +32,42 @@ document.addEventListener('DOMContentLoaded',()=>{
         if(data=== ''){
             alert("Please write something here");
         }else {
+            appendToDoInHtml(data)
             addToLocalStorage(data);
             todoInput.value="";
         }
+    });
+
+    const filterButton = document.getElementsByClassName('filterButton');
+    console.log(filterButton.item)
+    Array.from(filterButton).forEach((data)=> {
+        data.addEventListener('click',(event)=>{
+            const element = event.target;
+            const value = element.getAttribute("data-filter");
+            const todoList = document.getElementById('todo-list');
+            let dt = loadToDos();
+            todoList.innerHTML = '';
+            if ( value == 'all' ){
+                dt?.list?.forEach((todoText)=>{
+                    console.log(todoText)
+                    appendToDoInHtml(todoText.text);
+                });
+            }else if ( value == 'pending' ){
+                dt?.list?.forEach((todoText)=>{
+                    if(!todoText.isCompleted)
+                        appendToDoInHtml(todoText.text);
+                });
+            }else {
+                dt?.list?.forEach((todoText)=>{
+                    if(todoText.isCompleted)
+                        appendToDoInHtml(todoText.text);
+                });
+            }
+        });
     })
-    loadToDos();
+    let dt = loadToDos();
+    dt?.list?.forEach((todoText)=>{
+        console.log(todoText)
+        appendToDoInHtml(todoText.text);
+    });
 })
